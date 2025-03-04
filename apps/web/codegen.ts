b0d543e30c857e668d1ca104f8a210bc87a457d2
Diego Melendez <diedu89@gmail.com>
2025-03-04 13:42:27 +0100

    fix: include ts files for graphql types generation

--- apps/web/codegen.ts
+++ apps/web/codegen.ts
@@ -3,7 +3,7 @@ import type { CodegenConfig } from "@graphql-codegen/cli";
 const config: CodegenConfig = {
   overwrite: true,
   schema: "http://localhost:8000/graphql",
-  documents: "src/**/*.tsx",
+  documents: "src/**/*.{tsx,ts}",
   generates: {
     "src/lib/gql/": {
       preset: "client",
